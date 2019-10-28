93cbae48e5bd6f91f598c0bf227ce40e36aadae5
Samir Mejia <samir04m@gmail.com>
2019-10-28 18:16:50 -0500

    Buscardor terminado

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -65,7 +65,20 @@ export class HeroesService {
 
   public getHeroe(idx: string){
      return this.heroes[idx];
- }
+  }
+
+  public buscarHeroe(termino:string){
+     let heroesArr:Heroe[] = [];
+     termino = termino.toLowerCase();
+
+     for (let heroe of this.heroes){
+        let nombre = heroe.nombre.toLowerCase();
+        if(nombre.indexOf(termino) >= 0){
+           heroesArr.push(heroe);
+        }
+     }
+     return heroesArr;
+  }
 }
 
 export interface Heroe {
