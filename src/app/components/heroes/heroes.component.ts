1215b99c5d023bbd8f946415d5bff9d8f873279d
Samir Mejia <samir04m@gmail.com>
2019-10-28 11:49:09 -0500

    Creacion del componente heroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,7 +12,7 @@ export class HeroesComponent implements OnInit {
 
    heroes:Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService ) {
+  constructor(private _heroesService:HeroesService, private router:Router ) {
 
   }
 
@@ -19,5 +20,9 @@ export class HeroesComponent implements OnInit {
      this.heroes = this._heroesService.getHeroes();
      console.log(this.heroes);
   }
+
+  verHeroe(idx:number){
+     this.router.navigate(['/heroe',idx]);
+ }
 
 }
